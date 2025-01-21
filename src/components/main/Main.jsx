976f2061cb8ed8dde3a6ea4eff3488ebc455d687
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-21 09:51:28 +0530

    A

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,2 @@
+const response = await fetch("https://fakestoreapi.com/products");
+// https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json  // GeekTrust Api 
