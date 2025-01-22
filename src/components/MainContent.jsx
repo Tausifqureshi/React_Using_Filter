1de64424b0d342fc088bd5ac0452bbb4f7a24412
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-22 15:36:11 +0530

    Started-My-Project

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-const MainContent = () => {
-  return (
-    // <div className="flex-1 bg-gray-100 p-4">
-    //   <h1 className="text-2xl font-bold">Main Content</h1>
-    //   <p>Here goes the main content of the page.</p>
-    // </div>
-
-    <div className="flex-1 bg-gray-100 p-6">
-            <h1 className="text-2xl font-bold">Main Content Area</h1>
-            <p>Here is your main content!</p>
-            {/* <h1 className="text-green-500">Version: {version}</h1> */}
-          </div>
-  );
-};
+function MainContent() {
+  return <div>MainContent</div>;
+}
 
 export default MainContent;
