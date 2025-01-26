a717e47ccd0b5633758f00d6a7eacde49bbd75b4
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-26 10:45:17 +0530

    A

--- src/Redux/productSlice.jsx
+++ src/Redux/productSlice.jsx
@@ -1,6 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const productSlice = createSlice({
+    name: 'product',
+    initialState: {
+        products: []
+    },
+
+    reducers: {
+        
+    },
+
 
 
 })
+
+export default productSlice.reducer;
