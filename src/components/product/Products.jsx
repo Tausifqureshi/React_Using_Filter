47952550414d475b4cb34ef2ad5550437be00727
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-27 17:31:11 +0530

    Apprigte

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import {fetchProduct } from "../../Redux/productSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Products = () => {
   const dispatch = useDispatch();
+  const products = useSelector((state) => state.product.data);
 
   useEffect(() => {
     dispatch(fetchProduct());
@@ -55,13 +56,14 @@ const Products = () => {
     
   ];
 
-
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {productList.map((product) => (
+      {products.map((product) => (
         <div key={product.id} className="border p-4 rounded shadow">
-          <h3 className="text-lg font-bold">{product.name}</h3>
+          <img src={product.image} alt={product.title} className="w-full h-64 object-contain mix-blend-darken"/>
+          <h3 className="text-lg font-bold">{product.title.slice(0, 30)}</h3>
           <p className="text-gray-700">{product.price}</p>
           {/* <button onClick={() => {dispatch(fetchProducts(product))}} className="bg-blue-500 text-white px-4 py-2  mt-2 rounded">
             Add to Cart
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,44 @@
-  /** @type {import('tailwindcss').Config} */
-  export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    // plugins: [require('tailwind-scrollbar')],
+  // /** @type {import('tailwindcss').Config} */
+  // export default {
+  //   content: [
+  //     "./index.html",
+  //     "./src/**/*.{js,ts,jsx,tsx}",
+  //   ],
+  //   theme: {
+  //     extend: {},
+  //   },
+  //   // plugins: [require('tailwind-scrollbar')],
   
-    // variants: {
-    //   scrollbar: ['rounded'], // Enables rounded scrollbars
-    // },
-  }
+  //   // variants: {
+  //   //   scrollbar: ['rounded'], // Enables rounded scrollbars
+  //   // },
+  // }
+
+
+  // tailwind.config.js
+ export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Adjust this to your file structure
+  theme: {
+    extend: {
+      mixBlendMode: {
+        normal: 'normal',
+        multiply: 'multiply',
+        screen: 'screen',
+        overlay: 'overlay',
+        darken: 'darken',
+        lighten: 'lighten',
+        colorDodge: 'color-dodge',
+        colorBurn: 'color-burn',
+        hardLight: 'hard-light',
+        softLight: 'soft-light',
+        difference: 'difference',
+        exclusion: 'exclusion',
+        hue: 'hue',
+        saturation: 'saturation',
+        color: 'color',
+        luminosity: 'luminosity',
+      },
+    },
+  },
+  plugins: [],
+};
