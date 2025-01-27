219cfda7b3644378d6fb6b6828b4952eaa45a331
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-27 22:35:44 +0530

    Appwighte

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -62,8 +62,8 @@ const Products = () => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       {products.map((product) => (
         <div key={product.id} className="border p-4 rounded shadow">
-          <img src={product.image} alt={product.title} className="w-full h-64 object-contain mix-blend-darken"/>
-          <h3 className="text-lg font-bold">{product.title.slice(0, 30)}</h3>
+          <img src={product.image} alt={product.title} className="w-full h-48 object-contain mix-blend-darken  "/>
+          <h3 className="text-lg font-bold mt-2">{product.title}</h3>
           <p className="text-gray-700">{product.price}</p>
           {/* <button onClick={() => {dispatch(fetchProducts(product))}} className="bg-blue-500 text-white px-4 py-2  mt-2 rounded">
             Add to Cart
