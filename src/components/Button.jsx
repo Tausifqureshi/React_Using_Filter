1ac274686b1f4096073411f6251b0561b405c71b
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-28 17:34:19 +0530

    Dynamic-Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-function Button({ label, onClick, type = "button"}) {
-  return <div>
- 
-  <button onClick={onClick} type={type} className="bg-blue-500 text-white px-4 py-2  mt-2 rounded">
-    {label}
-  </button>
-  </div>;
+function Button({
+  children,
+  label,
+  onClick,
+  type = "button",
+  className,
+  ...props
+}) {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        type={type}
+        className={`bg-blue-500 text-white px-4 py-2 mt-2 rounded ${className}`}
+        {...props}
+      >
+         {children || label}
+      </button>
+    </div>
+  );
 }
 
 export default Button;
--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -22,20 +22,6 @@ const Products = () => {
 
 
   
-  // return (
-  //   <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-  //     {products.map((product) => (
-  //       <div key={product.id} className="border p-4 rounded shadow">
-  //         <img src={product.image} alt={product.title} className="w-full h-48 object-contain mix-blend-darken  "/>
-  //         <h3 className="text-lg font-bold mt-2">{product.title.slice(0, 25)}</h3>
-  //         <p className="text-gray-700">{product.price}</p>
-  //         <button  className="bg-blue-500 text-white px-4 py-2  mt-2 rounded">
-  //           Add to Cart
-  //         </button>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 };
 
 export default Products;
@@ -43,12 +29,3 @@ export default Products;
 
 
 
-// addToCart: (state, action) => {
-//   // state.cart.push(action.payload);
-//   // return [...state, action.payload]; // Add to cart mein jo bhi product aayega usko cart mein add karna.
-// return {
-// ...state, // Pura state ko copy karo
-// cart: [...state.cart, action.payload] // Purane cart ko copy karke naye product ko add karo
-// };
-
-// },
