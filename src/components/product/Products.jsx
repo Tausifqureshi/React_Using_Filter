5a22bd1307a697fc07d5eb871d0d83b2e495642d
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-28 10:38:18 +0530

    Filtering

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -17,7 +17,7 @@ const Products = () => {
           key={product.id}
           className="border rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 bg-white overflow-hidden"
         >
-          <div className="w-full bg-gray-100 " style={{ aspectRatio: '4/3' }}>
+          <div className="w-full bg-gray-100 " style={{ aspectRatio: '6/3' }}>
             <img
               src={product.image}
               alt={product.title}
@@ -40,34 +40,9 @@ const Products = () => {
     </div>
   );
 
-  // return (
-  //   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-  //     {products.map((product) => (
-  //       <div
-  //         key={product.id}
-  //         className="border rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 bg-white overflow-hidden"
-  //       >
-  //         <img
-  //           src={product.image}
-  //           alt={product.title}
-  //           className="w-full h-64 object-contain p-4 mix-blend-darken mx-auto"
-  //         />
-  //         <div className="p-4">
-  //           <h3 className="text-lg font-semibold text-gray-800 hover:text-blue-600 transition-colors truncate">
-  //             {product.title}
-  //           </h3>
-  //           <p className="text-xl font-bold text-green-600 mt-2">${product.price.toFixed(2)}</p>
-  //           <button
-  //             className="bg-yellow-500 hover:bg-yellow-600 text-white font-medium px-6 py-2 rounded-full mt-4 w-full transition-all duration-300"
-  //           >
-  //             Add to Cart
-  //           </button>
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 
+
+  
   // return (
   //   <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
   //     {products.map((product) => (
