0a21afd1a55c50feea39933d9741c96ec2800ab9
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-29 23:00:31 +0530

    AppLock

--- src/components/Context API/ProductProvider.jsx
+++ src/components/Context API/ProductProvider.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {createContext, useContext, useEffect} from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData} from '../../Redux/productSlice';
+
+const ProductContext = createContext();
+
 
 function ProductProvider() {
-  return <div>ProductProvider</div>;
+  const dispatch = useDispatch();
+  const { data, cart, loading, error } = useSelector((state) => state.product);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  
+  return (
+  <div>
+
+  </div>
+
+);
 }
 
+export const useProductContext = () => useContext(ProductContext);
 export default ProductProvider;
  
