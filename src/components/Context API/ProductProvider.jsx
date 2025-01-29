27432d1d4defb72bd35310e5ccba8f5955f2a95d
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-29 23:12:56 +0530

    App

--- src/components/Context API/ProductProvider.jsx
+++ src/components/Context API/ProductProvider.jsx
@@ -1,6 +1,6 @@
 import React, {createContext, useContext, useEffect} from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchData} from '../../Redux/productSlice';
+import { fetchData, addToCart, removeFromCart, clearCart } from '../../Redux/productSlice';
 
 const ProductContext = createContext();
 
@@ -13,12 +13,32 @@ function ProductProvider( {children}) {
     dispatch(fetchData());
   }, [dispatch]);
 
+
+const handleAddToCart = (product) => {
+  dispatch(addToCart(product));
+};
+
+const handleRemoveFromCart = (productId) => {
+  dispatch(removeFromCart(productId));
+}
+
+const handleClearCart = () => {
+  dispatch(clearCart());
+}
+
+
+
+
   return (
  <ProductContext.Provider value={{
    data: data,
    cart: cart,
    loading: loading,
-   jerror: error,
+   error: error,
+    handleAddToCart: handleAddToCart,
+    handleRemoveFromCart: handleRemoveFromCart,
+    handleClearCart: handleClearCart,
+    
 
  }}>
     {children}
