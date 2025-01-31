11a704f4cc5c1caeef5158cde49720720c447a98
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-31 12:01:10 +0530

    Context-Api

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -2,19 +2,24 @@ import React, { useEffect } from "react";
 import {fetchData } from "../../Redux/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Product from "./Product";
+import { useProductContext } from '../Context API/ProductProvider';
 
   
 const Products = () => {
   const dispatch = useDispatch();
-  const fetched_Products_Data = useSelector((state) => state.product.data);  
+  // const fetched_Products_Data = useSelector((state) => state.product.data);  
+  const { data, loading, error, addToCart } = useProductContext(); // Use context to get data, loading, error, and addToCart
 
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchData());
+  // }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 p-1">
-      {fetched_Products_Data .map((items)=>(
+      {data.map((items)=>(
         <Product key={items.id} {...items} />
       ))}
     </div>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 // import Sidebar from "../components/sidebar/Sidebar";
 import Products from "../components/product/Products";
-// import { useProductContext } from "../components/Context API/ProductProvider"; // Ensure correct import
 
 
 function Home() {
@@ -9,7 +8,7 @@ function Home() {
   
   return <div>
   {/* Home */} 
-  <h1>{key}</h1>
+
   <Products />
   </div>;
   
