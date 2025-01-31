8eefdc76fa9a79360f87f836fcbe6ff30547a188
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-31 10:21:10 +0530

    Context-API

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './index.css'
+import "./index.css";
 // import ProductProvider from "./components/Context API/ProductProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-
- <App /> 
-
-
-
-
-);
+root.render(<App />);
