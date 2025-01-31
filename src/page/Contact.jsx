184078584f73b6ad0c12474fb1c7dd8a1b6aaaf8
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-31 10:41:29 +0530

    Git

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,12 +1,4 @@
-// import React from "react";
-// import useProductContext from "../components/Context API/ProductProvider";
 
-// function Contact() {
-//   const {key} = useProductContext();
-//   return <div>Contact</div>;
-// }
-
-// export default Contact;
 
 
 import React from "react";
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Sidebar from "../components/sidebar/Sidebar";
+// import Sidebar from "../components/sidebar/Sidebar";
 import Products from "../components/product/Products";
+// import { useProductContext } from "../components/Context API/ProductProvider"; // Ensure correct import
+
 
 function Home() {
+  
+  
   return <div>
-  {/* Home */}
+  {/* Home */} 
+  <h1>{key}</h1>
   <Products />
   </div>;
   
