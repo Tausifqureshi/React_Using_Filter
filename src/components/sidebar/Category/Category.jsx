9b0ba0f28d73e6adee8137347c8543d431587468
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-03 09:43:12 +0530

    Category

--- src/components/sidebar/Category/Category.jsx
+++ src/components/sidebar/Category/Category.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../Input"
 import { useProductContext } from "../../Context API/ProductProvider";
 
 function Category({isSidebarOpen}) {
+    const [isSelected, setIsSelected] = useState([]);
     const produtsContex = useProductContext();
     console.log("produtsContex",produtsContex);
-    
+
   return( 
  <div className="flex  flex-col">
   <Input isSidebarOpen = {isSidebarOpen}/>
@@ -13,7 +14,7 @@ function Category({isSidebarOpen}) {
   <Input isSidebarOpen = {isSidebarOpen}/>
   <Input isSidebarOpen = {isSidebarOpen}/>
   <Input isSidebarOpen = {isSidebarOpen}/>
-  <h1>Category {produtsContex.App}</h1>
+ 
  
  </div>
   );
