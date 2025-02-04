5122ee874f154da8c315b90efe0d8143af6416ad
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-04 22:12:55 +0530

    App

--- src/components/sidebar/Category/Category.jsx
+++ src/components/sidebar/Category/Category.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../../Input"
 import { useProductContext } from "../../Context API/ProductProvider";
 
@@ -10,28 +10,71 @@ function Category({isSidebarOpen}) {
       const value = e.target.value;
       console.log("value", value);
       
-      selectedCategories.includes(value)
-        ? setSelectedCategories((prevSelectedCategories) =>
-            prevSelectedCategories.filter((c) => c !== value)
-          )
-        : setSelectedCategories((prevSelectedCategories) => [
-            ...prevSelectedCategories,
-            value,
-          ]);
+      // selectedCategories.includes(value)
+      //   ? setSelectedCategories((prevSelectedCategories) =>
+      //       prevSelectedCategories.filter((c) => c !== value)
+      //     )
+      //   : setSelectedCategories((prevSelectedCategories) => [
+      //       ...prevSelectedCategories,
+      //       value,
+      //     ]);
 
-      if (selectedCategories.length > 0) {
-        const filteredProducts = data.filter((product) =>
-          selectedCategories.includes(product.category)
-        );
-        setFilteredProducts(filteredProducts);
-      } else {
-        setFilteredProducts(data); // Show all products if no category selected
-      }
+     if(!selectedCategories.includes(value)){
+      setSelectedCategories((prevSelectedCategories) => [
+        ...prevSelectedCategories,  // Pura previous selectedCategories ko leke, naye category ko add karte hain
+        value,
+      ]);
+     }else{
+      setSelectedCategories((prevSelectedCategories) => prevSelectedCategories.filter((c) => c !== value));
+
+     }
 
     }
 
+
+    useEffect(()=>{
+
+      // if (selectedCategories.length > 0) {
+      //   const filteredProducts = data.filter((product) =>
+      //     selectedCategories.includes(product.category)
+      //   );
+      //   setFilteredProducts(filteredProducts);
+      // } else {
+      //   setFilteredProducts(data); // Show all products if no category selected
+      // }
+
+
+        //  const filteredProducts = selectedCategories.length > 0
+        //   ? data.filter((product) => selectedCategories.includes(product.category))
+        //   : data;
+        // setFilteredProducts(filteredProducts);
+
+
+        // First Data Render agar selectedCategories empty hai toh uska matlab user ne kuch select nahi kiya ya sirf spaces diye hain. Aise case me, hame saare products dikhane chahiye, isliye `data` ko directly set kar diya.
+        // const filteredProducts = selectedCategories.length === 0
+        // ? data
+        // : data.filter((product) => selectedCategories.includes(product.category));
+        // setFilteredProducts(filteredProducts);
+
+        
+        if(selectedCategories.length === 0){
+          setFilteredProducts(data);
+        }
+        else{
+          const filteredProducts = data.filter((product) => selectedCategories.includes(product.category));
+          setFilteredProducts(filteredProducts);
+        }
+
+
+
+
+    }, [data, selectedCategories, setFilteredProducts])
+
+
+
+
   return( 
- <div className="flex  flex-col">
+  <div className="flex flex-col">
    <h3>Filter by Category:</h3>
       {categories.map((category) => (
         <Input key={category} value={category} isSidebarOpen={isSidebarOpen}  
