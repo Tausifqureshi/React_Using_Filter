c872b0e465edbedf5d3d688feff44fb0050ab40e
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-05 10:51:43 +0530

    Category

--- src/components/sidebar/Category/Category.jsx
+++ src/components/sidebar/Category/Category.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
+import Input from "../../Input"
+import { useProductContext } from "../../Context API/ProductProvider";
 
 function Category({isSidebarOpen}) {
+const {data, setFilteredProducts} = useProductContext()
+const [selectedCategories, setSelectedCategories] = useState([]);
 
 
   return( 
