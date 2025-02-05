eb43f170e0198712e8e5eb30dd0cae8f0bf22471
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-05 11:00:23 +0530

    Category-Filter

--- src/components/sidebar/Category/Category.jsx
+++ src/components/sidebar/Category/Category.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Input from "../../Input"
 import { useProductContext } from "../../Context API/ProductProvider";
+import Product from "../../product/Product";
 
 function Category({isSidebarOpen}) {
 const {data, setFilteredProducts} = useProductContext()
 const [selectedCategories, setSelectedCategories] = useState([]);
 
+const categories = [...new Set(data.map((product)=>product.category))]
+
 
   return( 
   <div className="flex flex-col">
    <h3>Filter by Category:</h3>
+   {categories.map((category)=> <Input key={category} value={category} isSidebarOpen={isSidebarOpen}/>)}
  </div>
   );
 }
