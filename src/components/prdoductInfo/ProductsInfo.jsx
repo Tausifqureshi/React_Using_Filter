56aace473eda5d2c8fd4a7f2ba61b38d758fa006
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-16 10:17:33 +0530

    All

--- src/components/prdoductInfo/ProductsInfo.jsx
+++ src/components/prdoductInfo/ProductsInfo.jsx
@@ -1,11 +1,13 @@
-
 import React from "react";
+import { useProductContext } from "../Context API/ProductProvider";
+useProductContext
 
 function ProductsInfo() {
-  return <div> 
-     <h1> Products Info </h1>
-     
-   </div>;
+  return (
+    <div>
+      <h1> Products Info </h1>
+    </div>
+  );
 }
 
 export default ProductsInfo;
