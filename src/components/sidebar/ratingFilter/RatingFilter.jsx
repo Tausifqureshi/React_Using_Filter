ef620f05f94b80e8b9362b5b16526731583073c5
Tausifqureshi <tausifqureshi504@gmail.com>
2025-02-25 16:32:18 +0530

    Rating

--- src/components/sidebar/ratingFilter/RatingFilter.jsx
+++ src/components/sidebar/ratingFilter/RatingFilter.jsx
@@ -2,11 +2,27 @@ import Recat from "react";
 import { useProductContext } from "../../Context API/ProductProvider";
 
 function RatingFilter() {
-  const { ratingFilter, setRatingFilter, data, setFilteredProducts } = useProductContext();
-  
+  const { ratingFilter, setRatingFilter, data, setFilteredProducts } =
+    useProductContext();
+
+  const ratingFilterMap = [...new Set(data.map((item) => item.rating))];
+
   return (
     <div>
       <h1>Rating</h1>
+      {ratingFilterMap.map((rating) => (
+        <div key={rating}>
+          <input
+            type="checkbox"
+            id={rating}
+            name="rating"
+            value={rating}
+            // checked={ratingFilter.includes(rating)}
+          />
+          {/* returnPolicy : "30 days return policy" reviews */}
+          <label htmlFor={rating}>{rating}</label>
+        </div>
+      ))}
     </div>
   );
 }
