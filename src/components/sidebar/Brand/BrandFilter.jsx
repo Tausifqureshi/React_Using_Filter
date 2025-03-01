9a47322aa1b099bfa494c404fa61078fa07bf58c
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-01 09:41:00 +0530

    All

--- src/components/sidebar/Brand/BrandFilter.jsx
+++ src/components/sidebar/Brand/BrandFilter.jsx
@@ -3,11 +3,25 @@ import { useProductContext } from "../../Context API/ProductProvider";
 import Input from "../../Input";
 
 function BrandFilter() {
-    const { data,selectedBrand, setSelectedBrand, setFilteredProducts } = useProductContext();
+  const { data, selectedBrand, setSelectedBrand, setFilteredProducts } =
+    useProductContext();
+  const brand = [...new Set(data.map((item) => item.brand))];
 
-  return <div>
-   BrandFilter 
-  </div>;
+  return (
+    <div className="">
+      <h1>BrandFilter </h1>
+      {brand.map((brand) => (
+        <Input 
+          key={brand}
+          type="checkbox"
+          value={brand}
+          checked={selectedBrand.includes(brand)}
+        //   handleChange={}
+        />
+       
+      ))}
+    </div>
+  );
 }
 
 export default BrandFilter;
