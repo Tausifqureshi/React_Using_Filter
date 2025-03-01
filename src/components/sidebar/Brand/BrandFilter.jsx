d710c87c3916e3627d929e97450752ccc60d5eea
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-01 09:34:39 +0530

    Brand

--- src/components/sidebar/Brand/BrandFilter.jsx
+++ src/components/sidebar/Brand/BrandFilter.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useProductContext } from "../../Context API/ProductProvider";
+import Input from "../../Input";
 
 function BrandFilter() {
     const { data,selectedBrand, setSelectedBrand, setFilteredProducts } = useProductContext();
-    
-  return <div> BrandFilter </div>;
+
+  return <div>
+   BrandFilter 
+  </div>;
 }
 
 export default BrandFilter;
