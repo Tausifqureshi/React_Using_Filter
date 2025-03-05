e934ac370e6a21727aaffe2fe56177d7d0caa1c7
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-05 09:28:43 +0530

    BrnadFilter

--- ZZZ.js
+++ ZZZ.js
@@ -25,52 +25,52 @@
 
 
 
-    
-   import React, { useEffect } from "react";
-   import { useProductContext } from "../../Context API/ProductProvider";
-   import Input from "../../Input";
+  //BrandFilter Components code.
+  //  import React, { useEffect } from "react";
+  //  import { useProductContext } from "../../Context API/ProductProvider";
+  //  import Input from "../../Input";
    
-   function BrandFilter({ isSidebarOpen }) {
-     const { data, selectedBrand, setSelectedBrand, setFilteredProducts } =
-       useProductContext();
-     console.log(selectedBrand, "selectedBrand");
+  //  function BrandFilter({ isSidebarOpen }) {
+  //    const { data, selectedBrand, setSelectedBrand, setFilteredProducts } =
+  //      useProductContext();
+  //    console.log(selectedBrand, "selectedBrand");
      
-     const brand = [...new Set(data.map((item) => item.brand))];
-     const handleChange = (e) => {
-       console.log(e.target.value);
-       const targetValue = e.target.value;
-       if (selectedBrand.includes(targetValue)) {
-         setSelectedBrand((prev) => prev.filter((c) => c !== targetValue));
-       } else {
-         setSelectedBrand((prev) => [...prev, targetValue]);
-       }
-     };
+  //    const brand = [...new Set(data.map((item) => item.brand))];
+  //    const handleChange = (e) => {
+  //      console.log(e.target.value);
+  //      const targetValue = e.target.value;
+  //      if (selectedBrand.includes(targetValue)) {
+  //        setSelectedBrand((prev) => prev.filter((c) => c !== targetValue));
+  //      } else {
+  //        setSelectedBrand((prev) => [...prev, targetValue]);
+  //      }
+  //    };
          
-     useEffect(() => {
-       const filteredProducts =
-         selectedBrand.length === 0
-           ? data
-           : data.filter((item) => selectedBrand.includes(item.brand));
-       setFilteredProducts(filteredProducts);
-     }, [selectedBrand, data, setFilteredProducts]);
+  //    useEffect(() => {
+  //      const filteredProducts =
+  //        selectedBrand.length === 0
+  //          ? data
+  //          : data.filter((item) => selectedBrand.includes(item.brand));
+  //      setFilteredProducts(filteredProducts);
+  //    }, [selectedBrand, data, setFilteredProducts]);
    
-     return (
-       <div className="">
-         <h1>BrandFilter </h1>
-         {brand.map((value, index) => (
-           <Input
-             key={index}
-             type="checkbox"
-             value={value}
-             //   checked={selectedBrand.includes(value)}
-             isSidebarOpen={isSidebarOpen}
-             handleChange={handleChange}
-             checked={selectedBrand.includes(value)}
-           />
-         ))}
-       </div>
-     );
-   }
+  //    return (
+  //      <div className="">
+  //        <h1>BrandFilter </h1>
+  //        {brand.map((value, index) => (
+  //          <Input
+  //            key={index}
+  //            type="checkbox"
+  //            value={value}
+  //            //   checked={selectedBrand.includes(value)}
+  //            isSidebarOpen={isSidebarOpen}
+  //            handleChange={handleChange}
+  //            checked={selectedBrand.includes(value)}
+  //          />
+  //        ))}
+  //      </div>
+  //    );
+  //  }
    
-   export default BrandFilter;
+  //  export default BrandFilter;
    
