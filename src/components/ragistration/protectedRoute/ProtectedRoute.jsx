6e97dbe0a01f3582acfda8e7ee7d49980773f8c3
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-10 11:09:30 +0530

    ProtectedRouter

---
+++ src/components/ragistration/protectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProtectedRoute({ children }) {
+  return (
+    <>
+      <h1>ProtectedRoute</h1>
+      {children}
+    </>
+  );
+}
+
+export default ProtectedRoute;
