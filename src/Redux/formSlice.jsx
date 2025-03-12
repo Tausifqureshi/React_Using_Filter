b6eca086b4fe05577eddd4f05452d5d44a6b23a4
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-12 10:40:09 +0530

    Products

---
+++ src/Redux/formSlice.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import craeteSlice from "@reduxjs/toolkit";
+
+const formSlice = craeteSlice({
+  name: "form",
+  initialState: {
+   user: null
+  },
+  reducers: {
+
+    signup: (state, action) => {
+      state.user = action.payload;
+    },
+
+    loging: (state, action) => {
+      state.user = action.payload;
+    },
+
+    logout: (state) => {
+      state.user = null
+    }
+  },
+});
+
+export default formSlice.reducer;
--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./productSlice";
+import formSlice from "./formSlice";
 
 const store = configureStore({
   reducer: {
     product: productSlice,
+    form: formSlice,
   },
 });
 
