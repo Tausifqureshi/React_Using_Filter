249e3a0de2afd2f1567a71be4fff646f10572e55
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-20 10:28:23 +0530

    AllProducts

--- src/Redux/formSlice.jsx
+++ src/Redux/formSlice.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createSlice } from "@reduxjs/toolkit";
 
 const formSlice = createSlice({
-  name: "form",
+  name: "form",   
   initialState: {
     user: JSON.parse(localStorage.getItem("user")) || [],
     currentUser: JSON.parse(localStorage.getItem("currentUser")) || null,
