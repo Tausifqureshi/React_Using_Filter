80ffa65b7fe0d3afd058da142714dfbed35a9e51
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-28 17:41:22 +0530

    Signup

--- src/components/ragistration/Signup.jsx
+++ src/components/ragistration/Signup.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 
-
+                        
 function Signup() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
