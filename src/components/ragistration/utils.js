5f7b29f9406177fd57cace89276fd9a189ca275f
Tausifqureshi <tausifqureshi504@gmail.com>
2025-04-10 11:36:17 +0530

    Utilis

--- src/components/ragistration/utils.js
+++ src/components/ragistration/utils.js
@@ -1,25 +1,8 @@
-// export const validateEmail = (email) => {
-//   return String(email)
-//     .toLowerCase()
-//     .match(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//     );
-// };
-
-// export const validatePassword = (password) => {
-//   return String(password).match(
-//     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-//   );
-// };
-
-
-
-
 export const validateEmail = (email) => {
   return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 };
 
 export const validatePassword = (password) => {
   // return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password);
-  return password.length >= 8; // Password minimum 6 characters ka hona chahiye
+  return password.length >= 8; // Password minimum 8 characters ka hona chahiye
 };
